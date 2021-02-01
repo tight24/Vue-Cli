135509ffa0dcd0cfd26459d444001a45e0099358
taito <rsbull42@gmail.com>
2021-02-01 13:35:14 +0900

    vueclitutorial

---
+++ first/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <myheader></myheader>
+    <p v-if="msg.length > 0 &msg.length <=20">{{msg}}</p>
+    <p v-else-if="msg.length >20">too much words</p>
+    <p v-else>no text</p>
+    <input type="text" v-model="msg">
+    <br><br><button @click="clear()">clear</button>
+
+  </div>
+</template>
+
+<script>
+import myheader from './components/myheader.vue'
+
+export default{
+  components:{
+    myheader
+  },
+  data:function(){
+    return{
+      msg:'Hello Vue Cli!!'
+    }
+  },
+  methods:{
+    clear(){
+      this.msg=''
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ first/src/components/myheader.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    here is header
+  </div>
+</template>
+<script>
+export default {
+
+}
+</script>
+<style lang="stylus">
+
+</style>
