4cff3cc6c0672c44b7392e6d87047bc3be380917
taito <rsbull42@gmail.com>
2021-02-02 01:54:57 +0900

    add header

---
+++ messageboard/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <myheader></myheader>
+  </div>
+</template>
+
+<script>
+import myheader from './components/myheader.vue'
+
+export default {
+  components:{
+    myheader
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ messageboard/src/components/myheader.vue
@@ -0,0 +1,24 @@
+<template>
+<header id="app">
+  <h1>掲示板</h1>
+</header>
+</template>
+<script>
+export default {
+
+
+}
+</script>
+<style>
+header{
+  width: 100%;
+  height:auto;
+  background-color:rgb(184, 184, 184);
+
+}
+
+h1{
+  color: black;
+  padding-left: 10px;
+}
+</style>
