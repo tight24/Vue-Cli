167d31c76cbc0c00889b1d30b4a6304e72fe4624
taito <rsbull42@gmail.com>
2021-02-02 20:43:48 +0900

    message入力機能(非活性化あり)

---
+++ messageboard/src/views/Board.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="board">
+    <textarea  cols="50" rows="5"  v-model="message"></textarea>
+    <br><br>
+    <button :disabled="!isfull">Send</button>
+  </div>
+</template>
+<script>
+export default {
+  data:function(){
+    return {
+      message:''
+    }
+  },
+  computed:{
+    isfull:function(){
+      return this.message
+    }
+  }
+}
+</script>
