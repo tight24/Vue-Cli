dbf9eb1b6b21d4b03e03b402eab408b9e736d037
taito <rsbull42@gmail.com>
2021-02-02 18:54:08 +0900

    loginボタンの非活性化

--- messageboard/src/views/Login.vue
+++ messageboard/src/views/Login.vue
@@ -1,14 +1,32 @@
 <template>
   <div class="login">
     <p>Username</p>
-    <input type="text">
-    <p>password</p>
-    <input type="text">
-    <br><br><button>Login</button>
+    <input type="text" v-model="username">
+    <p>Password</p>
+    <input type="text" v-model="password">
+    <br><br><button :class="{inActive:isBlank}" :disabled="!isBlank">Login</button>
   </div>
 </template>
 <script>
 export default {
-  name:'Login'
+  name:'Login',
+  data:function(){
+    return{
+      username:'',
+      password:''
+    }
+  }
+  ,
+  computed:{
+        isBlank:function(){
+          return this.username && this.password
+        }
+}
 }
 </script>
+<style>
+.inActives{
+  pointer-events: none;
+  color: gray;
+}
+</style>
